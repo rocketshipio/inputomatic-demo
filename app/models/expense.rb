db5b8d62908a68cd060b33387efc71f1da8b9bc4
Brad Gessler <bradgessler@gmail.com>
2023-04-11 15:31:32 -0700

    Remove name so peeps don't add crazy stuff to demo

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,7 +4,4 @@ class Expense < ApplicationRecord
 
   validates :purchased_at,
     inclusion: Range.new(90.days.ago, 90.days.from_now)
-
-  validates :amount,
-    numericality: { greater_than_or_equal_to: 0 }
 end
--- db/migrate/20230328185544_create_expenses.rb
+++ db/migrate/20230328185544_create_expenses.rb
@@ -1,7 +1,6 @@
 class CreateExpenses < ActiveRecord::Migration[7.1]
   def change
     create_table :expenses do |t|
-      t.string :name
       t.datetime :purchased_at
       t.decimal :amount
 
